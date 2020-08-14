2a9843f4cb818757d776e2aef26e2f30946ed791
Damian Bielecki <bielik20@gmail.com>
2020-08-14 14:37:35 +0200

    Add clustering

--- node/test.js
+++ node/test.js
@@ -1,2 +1,8 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
-console.log( say("Node.js") );
+const { fit_draw } = require('../pkg/ssvm_nodejs_starter_lib.js');
+
+const fs = require('fs');
+
+console.log("iris cluster centers");
+var iris_csv = fs.readFileSync("iris.data.csv");
+var svg = fit_draw(iris_csv, 3, 800, 400, 50, "Demo");
+console.log(svg);
--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,196 @@
 use wasm_bindgen::prelude::*;
-use num_integer::lcm;
-use sha3::{Digest, Sha3_256, Keccak256};
-use serde::{Serialize, Deserialize};
+use tera::{Context, Tera};
+use std::str::FromStr;
+use std::iter::Iterator;
+use ndarray::{Array2};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Point {
-  x: f32,
-  y: f32
+#[derive(Clone, Debug)]
+pub struct Graph {
+    pub name: String,
+    pub size: usize,
+    pub points: Vec<Point>,
+    pub colour: String,
+    pub x_range: f64,
+    pub y_range: f64,
+    pub x_min: f64,
+    pub y_min: f64,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Line {
-  points: Vec<Point>,
-  valid: bool,
-  length: f32,
-  desc: String
+#[derive(Clone, Debug, Copy)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
 }
 
-#[wasm_bindgen]
-pub fn create_line (p1: &str, p2: &str, desc: &str) -> String {
-  let point1: Point = serde_json::from_str(p1).unwrap();
-  let point2: Point = serde_json::from_str(p2).unwrap();
-  let length = ((point1.x - point2.x) * (point1.x - point2.x) + (point1.y - point2.y) * (point1.y - point2.y)).sqrt();
+impl Graph {
+  pub fn new(name: String, colour: String) -> Self {
+      Graph {
+          name,
+          size: 0,
+          points: Vec::new(),
+          colour,
+          x_range : 0.,
+          y_range : 0.,
+          x_min : 0.,
+          y_min : 0.,
+      }
+  }
 
-  let valid = if length == 0.0 { false } else { true };
+  pub fn add_point(&mut self, x: f64, y: f64) {
+      self.points.push(Point { x, y });
+  }
 
-  let line = Line { points: vec![point1, point2], valid: valid, length: length, desc: desc.to_string() };
+  pub fn draw_svg(&self, width: usize, height: usize, padding: usize, path: Vec<Point>, centers: Vec<(f64, f64)>) -> String {
 
-  return serde_json::to_string(&line).unwrap();
-}
+    let mut context = Context::new();
 
-#[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  let r = String::from("hello ");
-  return r + s;
-}
+    let mut p: Vec<(f64, f64)> = Vec::new();
 
-#[wasm_bindgen]
-pub fn obfusticate(s: String) -> String {
-  (&s).chars().map(|c| {
-    match c {
-      'A' ..= 'M' | 'a' ..= 'm' => ((c as u8) + 13) as char,
-      'N' ..= 'Z' | 'n' ..= 'z' => ((c as u8) - 13) as char,
-      _ => c
+    for point in path {
+      p.push((point.x, point.y));
     }
-  }).collect()
+    //let min_x = graph.points.get(0).map(|val| val.x).unwrap_or(0.0);
+    /*let max_x = self
+      .points
+      .iter()
+      .map(|point| point.x)
+      .fold(0. / 0., f64::max);
+    //let min_y = graph.points.iter().map(|val| val.y).fold(0. / 0., f64::min);
+    let max_y = self
+      .points
+      .iter()
+      .map(|point| point.y)
+      .fold(0. / 0., f64::max);
+      //hardset the padding around the graph
+    */
+    // let c_str = CString::new(file).unwrap();
+    // let filename: *const c_char = c_str.as_ptr() as *const c_char;
+    // const filename: &str = file.clone();
+
+    //ensure the viewbox is as per input
+
+    context.insert("name", &self.name);
+    context.insert("width", &width);
+    context.insert("height", &height);
+    context.insert("padding", &padding);
+    context.insert("path", &p);
+    context.insert("centers", &centers);
+    context.insert("x_range", &self.x_range);
+    context.insert("y_range", &self.y_range);
+    context.insert("x_min", &self.x_min);
+    context.insert("y_min", &self.y_min);
+    context.insert("colour", &self.colour);
+    context.insert("lines", &10);
+
+    Tera::one_off(include_str!("graph.svg"), &context, true).expect("Could not draw graph")
+
+  }
+}
+
+pub fn generate_graph(xs: Vec<f64>, ys: Vec<f64>, title : &str) -> Graph {
+  let mut graph = Graph::new(title.into(), "#8ff0a4".into());
+  graph.size = xs.len();
+  for i in 0..graph.size {
+    graph.add_point(xs[i], ys[i]);
+  }
+  return graph;
 }
 
 #[wasm_bindgen]
-pub fn lowest_common_multiple(a: i32, b: i32) -> i32 {
-  let r = lcm(a, b);
-  return r;
+pub fn fit_draw (csv_content: &[u8], num_clusters: usize, width: usize, height: usize, padding: usize, title: &str) -> String {
+  let data: Vec<f64> = read_data(csv_content);
+  let mut xs: Vec<f64> = Vec::new();
+  let mut ys: Vec<f64> = Vec::new();
+  let mut tuples: Vec<(f64, f64)> = Vec::new();
+  let mut centers: Vec<(f64, f64)> = Vec::new();
+
+  let center_arr: Vec<f64> = fit (csv_content, num_clusters);
+  println!("{:?}", center_arr);
+
+  for i in 0..center_arr.len() {
+    if (i % 2) == 1 {
+      centers.push((center_arr[i-1], center_arr[i]));
+    }
+  }
+
+  for i in 0..data.len() {
+    if (i % 2) == 1 {
+      tuples.push((data[i-1], data[i]));
+    }
+  }
+
+  for i in 0..tuples.len() {
+    xs.push(tuples[i].0);
+    ys.push(tuples[i].1);
+  }
+
+  let mut graph = generate_graph(xs, ys, title);
+
+  let width = width - padding * 2;
+  let height = height - padding * 2;
+  //let min_x = graph.points.get(0).map(|val| val.x).unwrap_or(0.0);
+  let x_max = graph.points.iter().map(|point| point.x).fold(0. / 0., f64::max);
+  let x_min = graph.points.iter().map(|point| point.x).fold(0. / 0., f64::min);
+  let y_max = graph.points.iter().map(|point| point.y).fold(0. / 0., f64::max);
+  let y_min = graph.points.iter().map(|point| point.y).fold(0. / 0., f64::min);
+
+  graph.x_min = (x_min-1.0).round();
+  graph.y_min = (y_min-1.0).round();
+
+  graph.x_range = (x_max+1.0).round() - graph.x_min;
+  graph.y_range = (y_max+1.0).round() - graph.y_min;
+
+  //let min_y = graph.points.iter().map(|val| val.y).fold(0. / 0., f64::min);
+
+  let centers = centers
+                  .iter()
+                  .map(|val| ((val.0-graph.x_min) / graph.x_range * width as f64 + padding as f64,
+                       (val.1-graph.y_min) / graph.y_range * (height as f64 * -1.0) + (padding + height) as f64)).collect();
+
+  let path = graph
+              .points
+              .iter()
+              .map(|val| Point {
+                  //x: (val.x / graph.max_x * width as f64) + padding as f64,
+                  //y: (val.y / graph.max_y * (height as f64 * -1.0)) + (padding + height) as f64,
+                  x: ((val.x-graph.x_min) / graph.x_range * width as f64) + padding as f64,
+                  y: ((val.y-graph.y_min) / graph.y_range * (height as f64 * -1.0)) + (padding + height) as f64,
+              }).collect();
+            //  .enumerate()
+            //  .map(|(i, point)| {
+            //      if i == 0 {
+            //          format!("M {} {}", point.x, point.y)
+            //      } else {
+            //          format!("L {} {}", point.x, point.y)
+            //      }
+            //  })
+            //  .collect::<Vec<String>>().join(" ");
+
+  let out = graph.draw_svg(width, height, padding, path, centers);
+  return out;
 }
 
-#[wasm_bindgen]
-pub fn sha3_digest(v: Vec<u8>) -> Vec<u8> {
-  return Sha3_256::digest(&v).as_slice().to_vec();
+fn read_data(csv_content: &[u8]) -> Vec<f64> {
+  let mut data_reader = csv::Reader::from_reader(csv_content);
+  let mut data: Vec<f64> = Vec::new();
+  for record in data_reader.records() {
+      for field in record.unwrap().iter() {
+          let value = f64::from_str(field);
+          data.push(value.unwrap());
+      }
+  }
+  return data;
 }
 
-#[wasm_bindgen]
-pub fn keccak_digest(s: &[u8]) -> Vec<u8> {
-  return Keccak256::digest(s).as_slice().to_vec();
+pub fn fit (csv_content: &[u8], num_clusters: usize) -> Vec<f64> {
+    let data: Vec<f64> = read_data(csv_content);
+    let arr = Array2::from_shape_vec((data.len() / 2, 2), data).unwrap();
+    let (means, _clusters) = rkm::kmeans_lloyd(&arr.view(), num_clusters);
+
+    let mut serialized_vec : Vec<f64> = Vec::new();
+    for row in means.genrows() {
+      serialized_vec.push(row[0]);
+      serialized_vec.push(row[1]);
+    }
+    return serialized_vec;
 }
