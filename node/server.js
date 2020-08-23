f7f129f8d65b9ff6528af3dea9cd632b61b68057
Damian Bielecki <bielik20@gmail.com>
2020-08-23 13:11:17 +0200

    Adjust UI

--- node/server.js
+++ node/server.js
@@ -7,26 +7,23 @@ const port = 3000;
 app.use(express.static(__dirname + '/public'));
 app.use(fileUpload());
 
-app.get('/', (req, res) => res.redirect("/index.html"));
+app.get('/', (req, res) => res.redirect('/index.html'));
 
-app.post('/draw', function (req, res) {
+app.post('/report', function (req, res) {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
-  console.log ("Received " + req.files.json_file.name + " with size: " + req.files.json_file.size);
-  console.log ("Received " + req.body.commission_rate);
 
   const json_file = req.files.json_file;
   const assignments = JSON.parse(json_file.data.toString('ascii'));
   const commission_rate = req.body.commission_rate.toString();
 
-  console.log(assignments, commission_rate);
+  console.log('Received', { assignments, commission_rate });
   console.time(json_file.name);
   const result = create_report(assignments, commission_rate);
   console.timeEnd(json_file.name);
-  console.log(result);
+  console.log("Result", JSON.parse(result));
   res.send(result);
-})
+});
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
-
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
